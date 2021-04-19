e4381a2a4dda6852887d0dd4d2f77ce9cf63bd3d
Guillermo Martinez <dumblemoe@gmail.com>
2021-04-19 14:49:00 -0500

    Practicing order of operations in Java.

---
+++ OrderofOperations.java
@@ -0,0 +1,14 @@
+public class OrderofOperations {
+    public static void main(String[] args) {
+        // Order of Operations are based on priority
+        Float x = (1F+1F) * 2F / 3F + 4F - 5F;
+        System.out.println(x);
+    }
+}
+
+// Priority order in math:
+// 1. Paranthases ()
+// 2. Multiplication *
+// 3. Division /
+// 4.Addition +
+// 5.Subtraction -
