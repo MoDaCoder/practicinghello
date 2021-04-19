16e67926dd8f96d563fe3c914bcd9fbde1107543
Guillermo Martinez <dumblemoe@gmail.com>
2021-04-19 13:37:31 -0500

    Practicing on Constants in Java.

---
+++ src/Constants.java
@@ -0,0 +1,10 @@
+public class Constants {
+    public static void main(String[] args) {
+        // Use final to create a constant variable (Variable that can not be changed)
+        final float PI = 3.14F;
+        // PI = 1; Can't change the variable because it's a constant
+    }
+}
+
+// Use final to create a constant variable (Variable that can not be changed)
+// By conevention in Java always use all CAPS to name a variable
